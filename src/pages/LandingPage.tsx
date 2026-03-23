aeeb75e3c181654fc483034c63f3c1507dd42c03
lambbross <lambbross@gmail.com>
2026-03-23 11:32:29 +0100

    Replace Pro CTA with early access form

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import EarlyAccess from '../components/EarlyAccess';
 import { motion } from 'motion/react';
 import { 
   Search, 
